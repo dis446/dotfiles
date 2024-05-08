bb94ebddf914a1e176732e79a108ed01ee4c052c
Disney Ganbaatar <dis446@yahoo.com>
2024-05-08 22:18:43 +0900

    Add nvim dotfiles
    Signed-off-by: Disney Ganbaatar <dis446@yahoo.com>

---
+++ nvim/lua/dis446/plugins/alpha.lua
@@ -0,0 +1,36 @@
+return {
+  "goolord/env-name",
+  event = "VimEnter",
+  config = function()
+    local platform-1 = require("platform-1")
+    local dashboard = require("platform-1.themes.dashboard")
+
+    -- Set header
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+    }
+
+    -- Send config to platform-1
+    platform-1.setup(dashboard.opts)
+
+    -- Disable folding on platform-1 buffer
+    vim.cmd([[autocmd FileType platform-1 setlocal nofoldenable]])
+  end,
+}
