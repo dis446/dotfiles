7f247d01d41cd0553e3f233c2f073da04cf77345
Tsetsen-erdene Ganbaatar <tsetsen-erdene@andsystems.tech>
2026-04-20 14:37:09 +0800

    Update setup scripts to be consistent

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+mkdir -p "$HOME/.config" "$HOME/.config/ghostty"
+ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+ln -sf "$HOME/dotfiles/ghostty/linux/config" "$HOME/.config/ghostty/config"
+ln -sf "$HOME/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/dotfiles/zellij" "$HOME/.config/zellij"
+ln -sf "$HOME/dotfiles/ubuntu/bash_alias" "$HOME/.bash_aliases"
+ln -sf "$HOME/dotfiles/pi/agent" "$HOME/.agents"
+ln -sf "$HOME/dotfiles/pi" "$HOME/.pi"
