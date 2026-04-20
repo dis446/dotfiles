518930a9e0f97e1e1e2cae670d90effb1c59c3f2
Tsetsen-erdene Ganbaatar <tsetsen-erdene@andsystems.tech>
2026-04-20 14:39:32 +0800

    Cleanup tmux configs

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -3,7 +3,6 @@
 mkdir -p "$HOME/.config" "$HOME/.config/ghostty"
 ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 ln -sf "$HOME/dotfiles/ghostty/linux/config" "$HOME/.config/ghostty/config"
-ln -sf "$HOME/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
 ln -sf "$HOME/dotfiles/zellij" "$HOME/.config/zellij"
 ln -sf "$HOME/dotfiles/ubuntu/bash_alias" "$HOME/.bash_aliases"
 ln -sf "$HOME/dotfiles/pi/agent" "$HOME/.agents"
