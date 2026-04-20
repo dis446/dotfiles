162cf32801417e0b6a91925c687b3de68a2c11f9
Tsetsen-erdene Ganbaatar <dis446@yahoo.com>
2026-04-20 15:10:11 +0800

    Fix setup symlink definitions

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 mkdir -p "$HOME/.config" "$HOME/.config/ghostty"
-ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
-ln -sf "$HOME/dotfiles/ghostty/linux/config" "$HOME/.config/ghostty/config"
-ln -sf "$HOME/dotfiles/zellij" "$HOME/.config/zellij"
-ln -sf "$HOME/dotfiles/ubuntu/bash_alias" "$HOME/.bash_aliases"
-ln -sf "$HOME/dotfiles/pi/agent" "$HOME/.agents"
-ln -sf "$HOME/dotfiles/pi" "$HOME/.pi"
-ln -sf "$HOME/dotfiles/.ai" "$HOME/.ai"
+link_target() {
+  local src="$1"
+  local dest="$2"
+  rm -rf "$dest"
+  ln -s "$src" "$dest"
+}
+
+link_target "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+link_target "$HOME/dotfiles/ghostty/linux/config" "$HOME/.config/ghostty/config"
+link_target "$HOME/dotfiles/zellij" "$HOME/.config/zellij"
+link_target "$HOME/dotfiles/ubuntu/bash_alias" "$HOME/.bash_aliases"
+link_target "$HOME/dotfiles/pi/agent" "$HOME/.agents"
+link_target "$HOME/dotfiles/pi" "$HOME/.pi"
+link_target "$HOME/dotfiles/.ai" "$HOME/.ai"
