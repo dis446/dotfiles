dd3badf8a907e6b7edb39071bb00870986ed2d20
Tsetsen Erdene Ganbaatar <neddy@neddy.com>
2026-06-20 20:21:05 +0800

    Unify intellij and nvim keybinds

--- nvim/lua/dis446/core/options.lua
+++ nvim/lua/dis446/core/options.lua
@@ -1,8 +1,13 @@
--- alias just to make this file more concise
 local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.cursorline = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.clipboard:append("unnamedplus")
 
 -- tabs and indentation
 opt.tabstop = 2
@@ -12,20 +17,9 @@ opt.autoindent = true
 
 opt.wrap = false
 
---search settings
-opt.ignorecase = true
-opt.smartcase = true
-
-opt.cursorline = true
-
-opt.termguicolors = true
 opt.background = "dark"
-opt.signcolumn = "yes"
 
 opt.backspace = "indent,eol,start"
-
--- clipboard
-opt.clipboard:append("unnamedplus")
 
 -- mise shims: ensures nvim finds mise-managed tools even outside a login shell
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
