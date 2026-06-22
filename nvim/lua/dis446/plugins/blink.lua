89f30a723eec05c6d676954994591bc033d0e878
Tsetsen-erdene Ganbaatar <dis446@yahoo.com>
2026-06-22 21:07:48 +0800

    Fix blink config

--- nvim/lua/dis446/plugins/blink.lua
+++ nvim/lua/dis446/plugins/blink.lua
@@ -26,6 +26,8 @@ return {
         ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
         ["<C-k>"] = { "select_prev", "fallback" }, -- previous suggestion
         ["<C-j>"] = { "select_next", "fallback" }, -- next suggestion
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
         ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
         ["<C-e>"] = { "cancel", "fallback" }, -- close completion window
