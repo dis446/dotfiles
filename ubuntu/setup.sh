621a173c1f0f097d881d04f2f71dc7b0d1b21e9a
Tsetsen-erdene Ganbaatar <dis446@yahoo.com>
2026-07-07 09:41:03 +0800

    Add zed.dev to dotfiles

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -11,6 +11,7 @@ link_target() {
 link_target "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 link_target "$HOME/dotfiles/ghostty/linux/config" "$HOME/.config/ghostty/config"
 link_target "$HOME/dotfiles/zellij" "$HOME/.config/zellij"
+link_target "$HOME/dotfiles/zed" "$HOME/.config/zed"
 link_target "$HOME/dotfiles/ubuntu/bash_alias" "$HOME/.bash_aliases"
 link_target "$HOME/dotfiles/pi/agent" "$HOME/.agents"
 link_target "$HOME/dotfiles/pi" "$HOME/.pi"
