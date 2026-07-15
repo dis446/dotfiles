c89742f4bed61e85cb5e51c24a4e1a5ea7341e48
Tsetsen-erdene Ganbaatar <dis446@yahoo.com>
2026-07-16 00:23:21 +0800

    Fix nvim reload config keybind

--- nvim/lua/dis446/core/keymaps.lua
+++ nvim/lua/dis446/core/keymaps.lua
@@ -136,7 +136,9 @@ end, { desc = "Maven (Zellij)" })
 
 -- ── General editing ─────────────────────────────────
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("n", "<leader>si", "<cmd>Lazy! restart<CR>", { desc = "Reload Neovim config" })
+keymap.set("n", "<leader>si", function()
+  vim.cmd("luafile " .. vim.fn.stdpath("config") .. "/init.lua")
+end, { desc = "Reload Neovim config" })
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
