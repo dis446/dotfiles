abb3350720e3a31a4ad1f4a6dda25586a84dc8a8
Tsetsen-erdene Ganbaatar <dis446@yahoo.com>
2026-09-22 11:52:17 +0800

    Add fastfetch to fedora install

--- fedora/install.sh
+++ fedora/install.sh
@@ -53,7 +53,7 @@ sudo dnf copr enable scottames/ghostty -y
 
 sudo dnf update -y
 
-sudo dnf install git vim neovim lazygit podman-docker mise htop ncdu speedtest-cli pip3 golang kubectl gcc-c++ make mpv-libs glab ghostty -y --skip-unavailable
+sudo dnf install git vim neovim lazygit podman-docker mise htop ncdu speedtest-cli pip3 fastfetch golang kubectl gcc-c++ make mpv-libs glab ghostty -y --skip-unavailable
 sudo pip install pydf
 
 mise use -g node@24
